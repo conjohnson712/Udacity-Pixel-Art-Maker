42e46e6320ca77893bac53e6527699db9e5f84f2
conjohnson712 <80295208+conjohnson712@users.noreply.github.com>
2021-12-16 21:49:19 -0500

    Add files via upload
    Updated project after implementing Udacity reviewer suggestions

--- designs.js
+++ designs.js
@@ -1,37 +1,29 @@
 /*
-
-Becuase I imported jQuery, the $ symbol is now the same as the whole 
-line "document.getElementById" and "document.querySelector". The val() function returns the user
-input as a value that we can use.
-
+    Becuase I imported jQuery, the $ symbol is now the same as the whole 
+    line "document.getElementById" and "document.querySelector". The val() function returns the user
+    input as a value that we can use.
 */
-    
-    
-    
-
-"use strict";
 /* 
+    define DOM objects as variables so we can call them later
+    "use strict" + $ before a variable allows the same word to be used
+    as two different variables, as seen with height and $height
+*/
+"use strict";
 
-define DOM objects as variables so we can call them later
-"use strict" + $ before a variable allows the same word to be used 
-as two different variables, as seem with height and $height
-
-*/ 
-
-const $table = $('#pixelCanvas')
+const $table = $('#pixelCanvas');
 const $height = $('#inputHeight');
 const $width = $('#inputWidth');
 const $color = $('#colorPicker');
-    
+
 // Select size input
-$('#sizePicker').submit(event => {
+$('#sizePicker').submit(function(event) {
     //prevent the page from reloading when Submit is clicked
     event.preventDefault();
 
     //set the height and width parameters for makeGrid()
     let height = $height.val();
     let width = $width.val();
-    
+
     // make sure that the grid is cleared before each new entry
     $table.html('');
 
@@ -40,30 +32,30 @@ $('#sizePicker').submit(event => {
     paint();
 });
 
-       
+
 function makeGrid(height, width) {
-        
-    //loop through the table, appending rows (tr) based on height selection    
+
+    //loop through the table, appending rows (tr) based on height selection
     for (let a = 1; a <= height; a++) {
-        let row = $table.append('<tr></tr>'); 
+        let row = $table.append('<tr></tr>');
     };
-    
+
         //loop through the rows and add cells (td) in the row's innerHTML
         for (let b = 1; b <= width; b++) {
            let cell = $('tr').append('<td></td>');
         };
     };
-    
+
     // Select color input
     function paint() {
-        //search for td tags and register when they are clicked 
-        $('td').click( event => {
+        //search for td tags and register when they are clicked
+        $('td').click(function(event) {
 
             //define the color variable that will fill td's innerHTML
             let color = $color.val();
 
-            //select the current clicked cell, change background-color 
-            //in css to the user's selection 
+            //select the current clicked cell, change background-color
+            //in css to the user's selection
             $(event.currentTarget).css('background-color', color)
         })
     }
