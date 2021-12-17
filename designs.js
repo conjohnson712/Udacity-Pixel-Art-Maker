b5f6da4019c3657ed72688a00f1d4fb824435ac9
conjohnson712 <80295208+conjohnson712@users.noreply.github.com>
2021-12-16 22:11:44 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -10,24 +10,24 @@
 */
 "use strict";
 
-const $table = $('#pixelCanvas');
 const $height = $('#inputHeight');
 const $width = $('#inputWidth');
 const $color = $('#colorPicker');
+const $table = $('#pixelCanvas');
 
 // Select size input
 $('#sizePicker').submit(function(event) {
-    //prevent the page from reloading when Submit is clicked
+    // prevent the page from reloading when Submit is clicked
     event.preventDefault();
 
-    //set the height and width parameters for makeGrid()
+    // set the height and width parameters for makeGrid()
     let height = $height.val();
     let width = $width.val();
 
     // make sure that the grid is cleared before each new entry
     $table.html('');
 
-    //call makeGrid() and paint() to allow user interaction
+    // call makeGrid() and paint() to allow user interaction
     makeGrid(height, width);
     paint();
 });
@@ -35,27 +35,27 @@ $('#sizePicker').submit(function(event) {
 
 function makeGrid(height, width) {
 
-    //loop through the table, appending rows (tr) based on height selection
+    // loop through the table, appending rows (tr) based on height selection
     for (let a = 1; a <= height; a++) {
         let row = $table.append('<tr></tr>');
-    };
+    }
 
-        //loop through the rows and add cells (td) in the row's innerHTML
+        // loop through the rows and add cells (td) in the row's innerHTML
         for (let b = 1; b <= width; b++) {
            let cell = $('tr').append('<td></td>');
-        };
-    };
+        }
+    }
 
     // Select color input
     function paint() {
-        //search for td tags and register when they are clicked
+        // search for td tags and register when they are clicked
         $('td').click(function(event) {
 
-            //define the color variable that will fill td's innerHTML
+            // define the color variable that will fill td's innerHTML
             let color = $color.val();
 
-            //select the current clicked cell, change background-color
-            //in css to the user's selection
+            // select the current clicked cell, change background-color
+            // in css to the user's selection
             $(event.currentTarget).css('background-color', color)
         })
     }
